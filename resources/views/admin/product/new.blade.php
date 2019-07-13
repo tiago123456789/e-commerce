01dd374f790c37297ebb1b7e99af6d950f7fa252
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2019-07-13 13:30:17 -0300

    Put code preview image in file and call in page than necessary

--- resources/views/admin/product/new.blade.php
+++ resources/views/admin/product/new.blade.php
@@ -72,23 +72,6 @@
     </div>
 
     @section("js")
-        <script>
-            function readURL(input) {
-                $('#image_preview').removeClass("hidden");
-                if (input.files && input.files[0]) {
-                    const reader = new FileReader();
-
-                    reader.onload = function(e) {
-                        $('#image_preview').attr('src', e.target.result);
-                    };
-
-                    reader.readAsDataURL(input.files[0]);
-                }
-            }
-
-            $("input[type=file]").change(function() {
-                readURL(this);
-            });
-        </script>
+        <script src="{{ asset("js/previewImage.js") }}"></script>
     @stop
 @stop
